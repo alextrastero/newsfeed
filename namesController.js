bda0d72aaf6cd266739bcf911a8a0cbf481ad80c
Alexander Odell <alexander.odell@betvictor.com>
2014-09-17 17:08:42 +0200

    move to angularjs

---
+++ namesController.js
@@ -0,0 +1,8 @@
+function namesController($scope) {
+    $scope.names = [
+        {name:'Jani',money:33},
+        {name:'Hege',money:543},
+        {name:'Kai',money:11}
+    ];
+    $scope.countries = ['Afghanistan','Albania','Algeria','Andorra','Angola','Antigua and Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','The Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','Brunei','Bulgaria','Burkina Faso','Burma','Burundi'];
+}
