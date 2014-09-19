351243eaf595829d190c65f3fcb8309e4aee612a
Alexander Odell <alexander.odell@betvictor.com>
2014-09-19 11:20:37 +0200

    lost in space

--- addToJson.js
+++ addToJson.js
@@ -1,6 +1,5 @@
-function addToJson($scope, $http){
-  console.log('hell');
-  // $http.get('feed.json').success(function(response){
-  //   $scope.articles = response;
-  // })
+function feeder($scope, $http){
+  $http.get('feed.json').success(function(response){
+    $scope.articles = response;
+  })
 }
